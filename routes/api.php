539751f96683a7df4462c14b437e520acd50e0e1
chandraputra06 <chandra.wisesa06@gmail.com>
2026-04-13 17:35:58 +0800

    feat: add diagnosis history endpoint #7

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DiagnosaController;
 
+Route::get('/diagnoses', [DiagnosaController::class, 'index']);
 Route::post('/diagnose', [DiagnosaController::class, 'diagnose']);
